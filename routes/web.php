a002845b56b5115a3977eeef5fdf112b9c2aed44
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-05-06 15:35:41 +0800

    Progress: Finish Progress

--- routes/web.php
+++ routes/web.php
@@ -16,23 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', function () {
-    return view('dashboard.index', [
-        'page' => 'Dashboard',
-    ]);
-});
-
-Route::controller(SectionController::class)->group(function () {
-    Route::get('data-section', 'index');
-    Route::get('data-section/create', 'create');
-    Route::post('data-section/create', 'store');
-    Route::get('data-section/edit/{id}', 'edit');
-    Route::post('data-section/update/{id}', 'update');
-    Route::post('data-section/delete/{id}', 'destroy');
-});
-
-Route::controller(LoginController::class)->group(function () {
-    Route::get('login', 'index');
+Route::controller(LoginController::class)->middleware('guest')->group(function () {
+    Route::get('login', 'index')->name('login');
     Route::post('login', 'authenticate');
     Route::post('logout', 'logout');
 });
@@ -40,4 +25,21 @@ Route::controller(LoginController::class)->group(function () {
 Route::controller(RegisterController::class)->group(function () {
     Route::get('register', 'index');
     Route::post('register', 'register');
+})->middleware('guest');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard.index', [
+            'page' => 'Dashboard',
+        ]);
+    });
+
+    Route::controller(SectionController::class)->group(function () {
+        Route::get('data-section', 'index');
+        Route::get('data-section/create', 'create');
+        Route::post('data-section/create', 'store');
+        Route::get('data-section/edit/{id}', 'edit');
+        Route::post('data-section/update/{id}', 'update');
+        Route::post('data-section/delete/{id}', 'destroy');
+    });
 });
